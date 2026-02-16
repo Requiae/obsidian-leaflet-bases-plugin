82da87c99495821f3bf1a457dba9486ad093627e
Requiae <sara.slootmaker@gmail.com>
2026-02-16 10:44:46 +0100

    perty

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ export default BaseLeafletViewPlugin;
 // Remaining
 // TODO: Copy tool: right-click map get coordinates
 // TODO: Measure tool
-// TODO: Be able set the marker property identifier from settings
 // TODO: Map tool tooltips
 // TODO: Invalid map settings error (image not found)
-// BUG: Map zoom using scroll uses steps of 1, disregarding zoomDelta. Zoomdelta can't changed after init!?
+// BUG: Map zoom using scroll uses steps of 1, disregarding zoomDelta. Zoomdelta can't be changed after init!?
 
 // Maybe
+// TODO: Be able set the marker property identifier from settings
 // TODO: Pin tool: Right click map create note with marker?
 // TODO: Add marker type: pin | circle?
-// TODO: Prettier marker tooltip preview
+// TODO: Prettier marker tooltip note preview
